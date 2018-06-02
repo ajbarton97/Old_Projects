14bfb1e4252501ba62b3aaa8b841525a41ff2ca3
ajbarton97 <39846028+ajbarton97@users.noreply.github.com>
2018-06-01 21:54:22 -0400

    Create Brawlers.java

---
+++ Brawlers.java
@@ -0,0 +1,54 @@
+package server.model.players.content;
+
+import server.model.players.Client;
+
+/**
+ * @author Austin.
+ */
+
+public class Brawlers {
+
+	private final Client c;
+
+	int[][] GLOVES_SKILLS = 
+		{{13855, 13}, {13848, 5}, {13857, 7},
+		{13856, 10}, {13854, 17}, {13853, 21},
+		{13852, 14}, {13851, 11}, {13850, 8}};
+
+	public Brawlers(final Client client) {
+		super();
+		this.c = client;
+	}
+
+	public void handleExperienceGain(int skill, int amount) {
+		for (int i = 0; i < GLOVES_SKILLS.length; i++) {
+			if ((c.playerEquipment[c.playerHands] == GLOVES_SKILLS[i][0]) && (skill == GLOVES_SKILLS[i][1]))
+				c.getPA().addSkillXP(amount = (amount * 3), i);
+		}
+	}
+}
+
+/*
+package server.model.players.content;
+
+import server.model.players.Client;
+
+/**
+ * @author -Null Code-
+ */
+
+public class Brawlers {
+
+	private static int[][] GLOVES_SKILLS = 
+		{{13855, 13}, {13848, 5}, {13857, 7},
+		{13856, 10}, {13854, 17}, {13853, 21},
+		{13852, 14}, {13851, 11}, {13850, 8}};
+
+	public static void handleExperienceGain(Client c, int skill, int amount) {
+		for (int i = 0; i < GLOVES_SKILLS.length; i++) {
+			if ((c.playerEquipment[c.playerHands] == GLOVES_SKILLS[i][0]) && (skill == GLOVES_SKILLS[i][1]))
+				c.getPA().addSkillXP(amount = (amount * 3), i);
+		}
+	}
+}
+*/
